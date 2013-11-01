2b1ddff34c7e7fd5e0e85a39e2437a1c5e6e8864
LFDM <1986gh@gmail.com>
2013-11-01 21:06:41 +0100

    Some helper methods.

--- lib/experimental_graph.rb
+++ lib/experimental_graph.rb
@@ -1,5 +1,61 @@
 require "experimental_graph/version"
+require 'rubygems'
+require 'neography'
+require_relative '../config/initializers/neography'
+
+NEO = Neography::Rest.new
 
 module ExperimentalGraph
-  # Your code goes here...
+  # Bunch of methods for easier node creation
+
+  LABELS = %i{ person work lemma }
+  LABELS.each do |label|
+    self.class_eval <<-STR
+      def #{label}(name)
+        create_labeled(name, '#{label.capitalize}')
+      end
+    STR
+  end
+
+  def create_labeled(name, label)
+    n = NEO.create_node('name' => name)
+    NEO.add_label(n, label)
+    n
+  end
+
+  def n(args)
+    NEO.create_node(args)
+  end
+
+  # for additional properties
+
+  def props(node, args)
+    NEO.set_node_properties(node, args)
+  end
+
+  def label(node, label)
+    NEO.add_label(node, label)
+  end
+
+  # and for relations
+
+  def r(name, start_node, end_node)
+    NEO.create_relationship(name, start_node, end_node)
+  end
+
+  def contains(container, content)
+    r('contains', container, content)
+  end
+
+  def in_and_out(method, *nodes)
+    send(method, *nodes)
+    send(method, *nodes.reverse)
+  end
+
+  def method_missing(meth, *args, &blk)
+    case meth
+    when /^r_(.*)/ then NEO.create_relationship($1, *args)
+    else super
+    end
+  end
 end
