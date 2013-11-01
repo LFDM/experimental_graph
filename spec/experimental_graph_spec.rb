acf6be77f0b9c34e6873960b9ac4f1bb8ac1b3b2
LFDM <1986gh@gmail.com>
2013-11-01 10:55:49 +0100

    Remove default failing spec.

--- spec/experimental_graph_spec.rb
+++ spec/experimental_graph_spec.rb
@@ -4,8 +4,4 @@ describe ExperimentalGraph do
   it 'should have a version number' do
     ExperimentalGraph::VERSION.should_not be_nil
   end
-
-  it 'should do something useful' do
-    false.should be_true
-  end
 end
