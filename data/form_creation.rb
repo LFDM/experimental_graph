69fd1b049c5d66bfdeb5a81a3f193d28d5a8e741
LFDM <1986gh@gmail.com>
2013-11-02 23:37:10 +0100

    More form creation data.

--- data/form_creation.rb
+++ data/form_creation.rb
@@ -1,3 +1,4 @@
 CREATION = [
   { type: :noun, nom: 'Orgetorix', stem: 'Orgetorig', persona: true, iclass: 3, sexus: :m, options: { numerus: :sg } },
+  { type: :noun, nom: 'Helvetii', stem: 'Helveti', persona: true, iclass: 2, sexus: :m, options: { numerus: :pl } },
 ]
