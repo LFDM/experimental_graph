5457f04734bfc69116d71efff8303ac60cd1e15a
LFDM <1986gh@gmail.com>
2013-11-02 11:12:51 +0100

    Refactor, deletion of old code.

--- lib/experimental_graph.rb
+++ lib/experimental_graph.rb
@@ -26,6 +26,10 @@ module ExperimentalGraph
       Neography::Node.create(args)
     end
 
+    def rel(type, outgoing, ingoing)
+      Neography::Relationship.create(type, outgoing, ingoing)
+    end
+
     def node_with_label(label, args)
       n = node(args)
       @neo.add_label(n, label)
@@ -48,10 +52,6 @@ module ExperimentalGraph
       @neo.connection.get("/node/#{id}/labels")
     end
 
-    def rel(type, outgoing, ingoing)
-      Neography::Relationship.create(type, outgoing, ingoing)
-    end
-
     # Node retrieval
     def get_node(node)
       Neography::Node.load(node)
@@ -65,7 +65,6 @@ module ExperimentalGraph
 
     def convert(result)
       return if result.nil?
-
       data = result['data']
       cols = result['columns']
       data.map do |row|
@@ -79,6 +78,8 @@ module ExperimentalGraph
       if cell['type']
         Neography::Relationship.new(cell)
       elsif cell['self']
+        # need to add the server by hand, otherwise
+        # traversing with this node fails
         with_server(Neography::Node.new(cell))
       elsif cell.kind_of?(Enumerable)
         cell.map { |c| convert_cell(c) }
@@ -92,57 +93,4 @@ module ExperimentalGraph
       node
     end
   end
-  # Bunch of methods for easier node creation
-
-  LABELS = %i{ person work lemma ethnos }
-  LABELS.each do |label|
-    self.class_eval <<-STR
-      def #{label}(name)
-        create_labeled(name, '#{label.capitalize}')
-      end
-    STR
-  end
-
-  def create_labeled(name, label)
-    n = NEO.create_node('name' => name)
-    NEO.add_label(n, label)
-    NEO.add_node_to_index(label, label, name, n)
-    n
-  end
-
-  def n(args)
-    NEO.create_node(args)
-  end
-
-  # for additional properties
-
-  def props(node, args)
-    NEO.set_node_properties(node, args)
-  end
-
-  def label(node, label)
-    NEO.add_label(node, label)
-  end
-
-  # and for relations
-
-  def r(name, start_node, end_node)
-    NEO.create_relationship(name, start_node, end_node)
-  end
-
-  def contains(container, content)
-    r('contains', container, content)
-  end
-
-  def in_and_out(method, *nodes)
-    send(method, *nodes)
-    send(method, *nodes.reverse)
-  end
-
-  #def method_missing(meth, *args, &blk)
-    #case meth
-    #when /^r_(.*)/ then NEO.create_relationship($1, *args)
-    #else super
-    #end
-  #end
 end
