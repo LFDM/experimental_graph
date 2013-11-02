f4837bc770b757724f648b78865e0040c80240f1
LFDM <1986gh@gmail.com>
2013-11-02 12:07:46 +0100

    Require xml parser was missing.

--- lib/experimental_graph.rb
+++ lib/experimental_graph.rb
@@ -1,8 +1,6 @@
 require "experimental_graph/version"
-require 'rubygems'
-require 'neography'
-require_relative '../config/initializers/neography'
 
 module ExperimentalGraph
   require 'neo4j'
+  require 'xml_parser'
 end
--- lib/xml_parser.rb
+++ lib/xml_parser.rb
@@ -1,5 +1,10 @@
 require 'nokogiri'
 
 class XmlParser
+  CAESAR_PATH = File.expand_path('..data/caesar.xml', __FILE__)
 
+  def initialize
+    @doc = Nokogiri::XML(CAESAR_PATH)
+    require 'pry'; binding.pry
+  end
 end
