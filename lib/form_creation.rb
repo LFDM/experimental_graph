2be8d0fdd2bcbe3d92f1e073505b3dc4eb1c62c2
LFDM <1986gh@gmail.com>
2013-11-17 12:09:59 +0100

    Better abstractions in FormCreation.
    To handle stems.

--- lib/form_creation.rb
+++ lib/form_creation.rb
@@ -19,21 +19,23 @@ class FormCreation
     @builder.build(*args)
   end
 
-  def create_forms(forms)
+  def create_forms(forms, with_stem = false)
     nodes = create_nodes(forms)
     lemma_string = forms.first.to_s
     lemma = find_or_create_lemma(lemma_string)
     nodes.zip(forms).each do |node, form|
-      create_morphological_edges(node, form, lemma)
+      create_morphological_edges(node, form, lemma, with_stem)
     end
   end
 
-  def create_morphological_edges(node, form , lemma)
+  # this will only work with nouns at the moment
+  def create_morphological_edges(node, form , lemma, with_stem)
     lemma.outgoing(:form) << node
     edges = edges_through_type(form)
+    target = target_of_morph_edges(form, lemma, with_stem)
     edges.each do |edge|
       val = normalized_val(form, edge)
-      lemma.outgoing(val) << node
+      target.outgoing(val) << node
     end
   end
 
@@ -44,22 +46,40 @@ class FormCreation
     Hash[norm_values.zip(abbr_values)][val]
   end
 
+  def target_of_morph_edges(form, lemma, with_stem)
+    if with_stem
+      stem = find_or_create_distinct_node(:stem, form.stem)
+      handle_stem_edge(stem, lemma)
+      stem
+    else
+      lemma
+    end
+  end
+
+  def handle_stem_edge(stem, lemma)
+    # don't create an edge if its already here
+    lemma.outgoing(:stem) << stem
+  end
+
   def edges_through_type(form)
     case form.class.name
     when /Noun$/ then %i{ casus numerus sexus }
     end
   end
 
-  def find_or_create_lemma(lemma_string)
-    l = @neo.find('lemma', lemma_string).first
-    return l if l
-    create_lemma(lemma_string)
+  def find_or_create_distinct_node(type, string)
+    index = type.to_s
+    n = @neo.find(index, string).first
+    return n if n
+    create_distinct_node(string)
   end
 
-  def create_lemma(lemma_string)
-    l = @neo.node_with_label('Lemma', name: lemma_string)
-    @neo.add_index(l, 'lemma', lemma_string)
-    l
+  def create_distint_node(type, string)
+    index = type.to_s
+    label = index.capitalize
+    n = @neo.node_with_label(label, name: string)
+    @neo.add_index(l, index, string)
+    n
   end
 
   def create_nodes(forms)
