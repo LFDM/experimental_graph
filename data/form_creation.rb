6b0b23545c86980a3e123b058dff80a4b97ec88c
LFDM <1986gh@gmail.com>
2013-11-19 13:36:39 +0100

    Data fix.

--- data/form_creation.rb
+++ data/form_creation.rb
@@ -2,5 +2,5 @@ CREATION = [
   { type: :noun, nom: 'Orgetorix', stem: 'Orgetorig', persona: true, iclass: 3, sexus: :m, options: { numerus: :sg } },
   { type: :noun, nom: 'Helvetii', stem: 'Helveti', persona: true, iclass: 2, sexus: :m, options: { numerus: :pl } },
   { type: :noun, nom: 'homo', stem: 'homin', iclass: 3, sexus: :m },
-  { type: :noun, stem: 'templ', iclass: 1, sexus: :n },
+  { type: :noun, stem: 'templ', iclass: 2, sexus: :n },
 ]
